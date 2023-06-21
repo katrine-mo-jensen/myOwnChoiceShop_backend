ad9938fea57dc194f91038964e34ae9cc34074f4
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-06-21 15:02:17 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'; 
 import { productRouter } from './routers/product.route.js'
-import BrandRouter from './routers/brand.router.js'
+import { BrandRouter } from './routers/brand.router.js'
+import { router as InitRouter } from './routers/init.sequelize.route.js'
 
 
 
@@ -12,20 +13,22 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 app.use(productRouter);
-app.use(BrandRouter)
+app.use(BrandRouter),
+app.use(InitRouter);
 
 
-// app.get("/", (req, res) => {
-//   res.send("Hej verden fuck!");
-// });
 
-// app.get("/about", (req, res) => {
-//   res.send("Læs mere om min Node.js app!");
-// });
+app.get("/", (req, res) => {
+  res.send("Hej verden fuck!");
+});
 
-// app.use((req, res, next) => {
-//     res.status(404).send("Siden blev ikke fundet fuck");
-//   });
+app.get("/about", (req, res) => {
+  res.send("Læs mere om min Node.js app!");
+});
+
+app.use((req, res, next) => {
+    res.status(404).send("Siden blev ikke fundet fuck");
+  });
 
 app.listen(process.env.PORT, () => {
     console.log(`Server kører på http://localhost:${process.env.PORT}`);
