72d3f32d6c3366883e84713e7f0f416fc83fee43
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-06-23 11:46:31 +0200

    update

--- models/brand.model.js
+++ models/brand.model.js
@@ -1,5 +1,5 @@
 import { DataTypes, Model } from 'sequelize';
-import { sequelize } from "../config/db.sequelize.js";
+import  sequelize  from "../config/db.sequelize.js";
 
 class BrandModel extends Model {}
 
