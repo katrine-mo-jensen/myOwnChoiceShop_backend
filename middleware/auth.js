9de141fde45c903da699980db7daa8c8a098b38b
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-06-23 13:52:01 +0200

    user update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,17 @@
+import UserModel from '../models/user.model.js'
 const Authenticate = async (req, res) => { 
+    const { username, password } = req.body
+
+    if(username && password) {
+        const user_result = await UserModel.findOne({
+            // attributes: {"id", "firstname", "lastname", "password"},
+            // where: { email: username, is_active: 1}
+        })
+
+    } else {
+        console.log('Du skal udfylde alle felter')
+
+    }
 
 }
 
--- routers/auth.router.js
+++ routers/auth.router.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import { Authenticate, Authorize } from '../middleware/auth.js'
+
 const AuthRouter = express.Router()
 
-AuthRouter.post('/login', (req, res) => { 
-    console.log(req.body)
-    })
+AuthRouter.post('/login', (req, res) => {
+  Authenticate(req, res)
+})
 
 export default AuthRouter
